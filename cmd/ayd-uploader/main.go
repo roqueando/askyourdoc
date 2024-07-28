07af2b734fd95378d55800750b2fd7e3619d2a61
Vitor Roque <vitor.roquep@gmail.com>
2024-07-28 19:36:59 -0300

    feat: adding uploader

--- cmd/ayd-uploader/main.go
+++ cmd/ayd-uploader/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "log"
+)
+
+func ask(w http.ResponseWriter, r *http.Request) {
+  r.ParseMultipartForm(10 << 20)
+  file, handler, err := r.FormFile("doc")
+
+  if err != nil {
+    log.Println("error retrieving the file")
+    log.Fatal(err)
+    return
+  }
+
+  defer file.Close() // close file when function ends
+
+  mb := float64(handler.Size / 1048576)
+  fmt.Printf("%s has %f MBs\n", handler.Filename, mb)
+
+  fmt.Fprintf(w, "Hello there!")
+}
 
 func main() {
-  fmt.Println("hello from ayd-uploader")
+  http.HandleFunc("/ask", ask)
+  log.Println("[ayd-uploader] up and running on port 8080")
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
